65e9b254ffe20cf3dee6a996e2946844a79cc984
Daniel Bergeron <104526289+danbergeron3@users.noreply.github.com>
2023-12-22 11:21:53 -0500

    started work

--- Sheet-Generator.py
+++
@@ -1,9 +0,0 @@
-from openpyxl import Workbook
-
-
-tb = Workbook()
-ts = tb.active
-
-ts = tb.create_sheet("test sheet")
-print("sheet created")
-tb.save("test-sheet.xlsx")
---
+++ classes/week-class.py
@@ -0,0 +1,1 @@
+from openpyxl import Workbook
