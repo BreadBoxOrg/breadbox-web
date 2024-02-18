57cd8ae0bd17215f9201f8d0343f78334f659400
callbacked <alexanderjunior120@gmail.com>
2024-02-18 03:33:13 +0000

    preparing for merge

--- breadbox/src/App.js
+++ breadbox/src/App.js
@@ -1,21 +1,11 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+import React from "react";
+import NavBar from "./routes/NavBarRoutes.js";
+import './index.css'
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div className="app-container">
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Navigate to="/Dashboard" />} /> {/* dashboard redirect*/}
-              <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-    </div>
-  );
+  return (<>
+          <NavBar />
+          </>);
 }
 
 export default App;
