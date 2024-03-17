d84bb8a42f04034ee77fb3e84cc6a449357d7bd7
Shivam Patel <smpatel0423@gmail.com>
2024-03-16 21:32:40 -0400

    made chatbot.

---
+++ Frontend/src/components/CustomChatStep.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { sendMessage } from '../utils/sendchat'; 
+
+class CustomChatStep extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { loading: false, message: '' };
+    }
+
+    componentDidMount() {
+        const { steps } = this.props;
+        const userMessage = steps.userInput.value; 
+        this.sendMessageToBackend(userMessage);
+    }
+
+    async sendMessageToBackend(message) {
+        this.setState({ loading: true });
+        try {
+            const responseMessage = await sendMessage(message);
+            this.setState({ loading: false, message: responseMessage });
+            this.triggerNextStep();
+        } catch (error) {
+            console.error("Error sending message:", error);
+            this.setState({ loading: false, message: "Error communicating with the backend." });
+            this.triggerNextStep();
+        }
+    }
+
+    triggerNextStep() {
+        // Trigger the next step in the chatbot
+        this.props.triggerNextStep({ trigger: 'nextStep' }); 
+    }
+
+    render() {
+        const { loading, message } = this.state;
+
+        if (loading) {
+            return <div>Sending message...</div>;
+        }
+
+        return (
+            <div>{message}</div>
+        );
+    }
+}
+
+export default CustomChatStep;
