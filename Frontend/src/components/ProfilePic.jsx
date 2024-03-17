b3184e3e8f45833ad3d44cf1db1aef1a90eb1b9b
jordyguz10 <Jordy_GuzmanAlcaraz@student.uml.edu>
2024-03-17 11:22:14 -0400

    added new profile pic component

---
+++ Frontend/src/components/ProfilePic.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function ProfilePicChange({ profilePic, onProfilePicChange, isEditing}) {
+  const handleProfilePicChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      const imageDataUrl = reader.result;
+      onProfilePicChange(imageDataUrl); // Pass the image data URL to the parent component
+      // Store the image data URL in sessionStorage
+      sessionStorage.setItem('profilePic', imageDataUrl);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const style= {
+        cursor: isEditing ? 'pointer' : 'auto',
+        border: isEditing ? '2px solid #007bff' : 'none' // Apply border when editing
+      }
+
+  return (
+    <div className="profile-pic-change">
+      <label htmlFor="profilePicInput">
+        <img className="settings-profilePic" src={profilePic} alt="placeholder" style={style} />
+        <input
+          id="profilePicInput"
+          type="file"
+          accept="image/*"
+          onChange={handleProfilePicChange}
+          style={{ display: 'none', cursor: 'pointer'}}
+          disabled={!isEditing}
+        />
+      </label>
+    </div>
+  );
+}
+
+export default ProfilePicChange;
