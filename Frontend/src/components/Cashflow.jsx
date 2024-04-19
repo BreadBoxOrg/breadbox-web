ba0d1cf7504a23f0280d9e6c4d38a296b9ecf55b
callbacked <alexanderjunior120@gmail.com>
2024-04-19 20:32:35 +0000

    slighty adjustment

--- Frontend/src/components/Cashflow.jsx
+++ Frontend/src/components/Cashflow.jsx
@@ -40,16 +40,16 @@ function CashFlow() {
       </div>
       {isLoading ? (
           <div role="status" class="w-full p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700" style={{ height: '250px' }}>
-            <div class="flex items-center justify-center w-2/3 h-12 mb-4 bg-gray-300 rounded dark:bg-gray-700">
-          </div>
-              <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-              <div class="flex items-center mt-4">
-            <div>
-                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
-                <div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+              <div class="flex items-center justify-center w-2/3 h-12 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+            </div>
+                <div class="h-1.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                <div class="flex items-center mt-4">
+              <div>
+                  <div class="h-1.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
+                  <div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
             </div>
         </div>
         <span class="sr-only">Loading...</span>
